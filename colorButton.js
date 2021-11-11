89eb547d995ef469bb0095e7f6fc392cd86d9fae
Mateichuk_Oleksandr <71691100+Meziran@users.noreply.github.com>
2021-11-11 10:40:23 +0200

    +colorButton

--- colorButton.js
+++ colorButton.js
@@ -1,4 +1,4 @@
 jQuery(document).on( 'click', 'button', function() {  
     jQuery('button').removeClass("active-btn");
-  jQuery(this).addClass(".active-btn");
+  jQuery(this).addClass("active-btn");
 });
